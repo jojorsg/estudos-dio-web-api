c3e1e635437ac69989f5a143ab55125ed915b384
jojorsg <josueroberto099@gmail.com>
2023-10-09 16:41:56 -0300

    Implementando spring security na API

--- src/main/java/jojo/web/api/controller/WelcomeController.java
+++ src/main/java/jojo/web/api/controller/WelcomeController.java
@@ -1,5 +1,6 @@
 package jojo.web.api.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,15 @@ public class WelcomeController {
     @GetMapping
     public String welcome() {
         return "Welcome to My Spring Boot Web API!";
+    }
+
+    @GetMapping("/users")
+    public String users() {
+        return "Authorized user";
+    }
+
+    @GetMapping("/managers")
+    public String managers() {
+        return "Authorized manager";
     }
 }
